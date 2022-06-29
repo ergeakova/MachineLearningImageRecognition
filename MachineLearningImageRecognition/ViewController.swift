8d6400aac6dd8ccbfdbeff845b13bd389b324b68
ergeakova <ergeak@gmail.com>
2022-06-29 16:26:07 +0300

    added request func

--- MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-    
     @IBOutlet weak var lblResult: UILabel!
+    var chosenImage = CIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,26 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         imageView.image = info[.originalImage] as? UIImage
         self.dismiss(animated: true)
+        
+        recognizeImage(image: imageView.image!)
+    }
+    
+    func recognizeImage(image: UIImage){
+        if let ciImage = CIImage(image: image){chosenImage = ciImage}
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model){
+            let request = VNCoreMLRequest(model: model) { vnRequest, error in
+                if let results = vnRequest.results as? [VNClassificationObservation]{
+                    if results.count > 0 {
+                        let topResult = results.first
+                        
+                        DispatchQueue.main.async {
+                            let confidenceLevel = (topResult?.confidence ?? 0)  * 100
+                            self.lblResult.text = "\(confidenceLevel) - \(topResult?.identifier)"
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
